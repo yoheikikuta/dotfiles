bff0c58c4615d01f5071f30193069caa1b4971bd
yoheikikuta <diracdiego@gmail.com>
2018-05-13 21:07:53 +0900

    Add symbolic link for .ideavimrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,4 @@ git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Sh
 ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/git/dotfiles/.dein.toml ~/.dein.toml
 ln -sf ~/git/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
+ln -sf ~/git/dotfiles/.ideavimrc ~/.ideavimrc
