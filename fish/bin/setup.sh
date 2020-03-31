a4b1b2404c92a56d7f8474846e0f7c9441e576a7
yoheikikuta <diracdiego@gmail.com>
2020-03-31 17:42:48 +0900

    [Update] Check if fisher exists

--- fish/bin/setup.sh
+++ fish/bin/setup.sh
@@ -1,7 +1,12 @@
+#! /bin/sh
 
-
-curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
-fish -C 'fisher add oh-my-fish/theme-bobthefish oh-my-fish/plugin-peco jethrokuan/z 0rax/fish-bd'
+DIR="$HOME/.config/fisher/"
+if [ -d "$DIR" ]; then
+    echo "fisher is already installed."
+else
+    curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+    fish -C 'fisher add oh-my-fish/theme-bobthefish oh-my-fish/plugin-peco jethrokuan/z 0rax/fish-bd'
+fi
 
 rm -rf c $HOME/.config/fish/config.fish
 ln -s $HOME/.dotfiles/fish/config.d/config.fish $HOME/.config/fish/config.fish
