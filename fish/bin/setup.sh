e12c79f7ccec8519efd09c9db89c0afd938c6767
Yohei Kikuta <yoheikikuta@Yoheis-MacBook-Air.local>
2021-09-05 17:33:40 +0900

    [tmp] Fix to use fiser install

--- fish/bin/setup.sh
+++ fish/bin/setup.sh
@@ -5,7 +5,7 @@ if [ -d "$DIR" ]; then
     echo "fisher is already installed."
 else
     curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
-    fish -C 'fisher add oh-my-fish/theme-bobthefish oh-my-fish/plugin-peco jethrokuan/z 0rax/fish-bd'
+    fish -C 'fisher install oh-my-fish/theme-bobthefish oh-my-fish/plugin-peco jethrokuan/z 0rax/fish-bd'
 fi
 
 rm -rf c $HOME/.config/fish/config.fish
