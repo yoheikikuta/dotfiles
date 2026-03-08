471736d8282242397da10f1850baf742aa248b7c
yoheikikuta <diracdiego@gmail.com>
2026-03-08 22:59:27 +0900

    Add git config and use 1password as SSH agent

--- modules/home.nix
+++ modules/home.nix
@@ -11,6 +11,22 @@
     enable = true;
     userName = "yoheikikuta";
     userEmail = "diracdiego@gmail.com";
+    signing = {
+      key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkCkwlP4g9fje5DKsPmLv4XYPr+aFreDLJkzoENd0Q4Pjy4HBgucYKoPMP99lqdwbPo/F8AbXVSGX/xe5Jq41ptQVwQAAWBAsi0kUtiBpGjEoCdsP7MxJierFsZTEm0zOXd/qTMdDAbes242mUYP4nCjlmPV+Pcid5KBlXzwVkqxf3PhSE8Qp09mQqbb9Z8shlHSESkpHALnodkYmFVHg5ffeKssHZqnu3Qy05WPMLEjrXXVMaqFCFYJ0XlR5S54eDsYRV/lZiYju38MliUBEPgaGQ2Y0rjUPuShomHFEP4FYymeShS/Wt7v1GHerfkuvpLz/HI+WomxMabOEGo5KqAjQyzyW+0UOgX7NXvo6XK2Ujsca8QYTADe+B86sxRqcq46wDhh9j+7XlnbbRle6FRG/PjG9tE0SieNCmYnmDUaOh8apGRvP7srhA6WAx/QZMIW4J3ffAO8Dq9ZEdGsR0BQA+yq+d40aWpAoIwZSJ8YCkRHfm5vRPWZceazNKxD8=";
+      signByDefault = true;
+    };
+    extraConfig = {
+      gpg.format = "ssh";
+      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    };
+  };
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    '';
   };
 
   programs.home-manager.enable = true;
