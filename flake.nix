6e7c3db02fd21a6e5fb0cbd25c7f3225d6deff3d
yoheikikuta <diracdiego@gmail.com>
2026-03-09 21:33:36 +0900

    test: verify commit signing

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.users.yoheikikuta = import ./modules/home.nix;
         }
       ];
--- modules/home.nix
+++ modules/home.nix
@@ -11,11 +11,13 @@
     enable = true;
     userName = "yoheikikuta";
     userEmail = "diracdiego@gmail.com";
-    signing = {
-      key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkCkwlP4g9fje5DKsPmLv4XYPr+aFreDLJkzoENd0Q4Pjy4HBgucYKoPMP99lqdwbPo/F8AbXVSGX/xe5Jq41ptQVwQAAWBAsi0kUtiBpGjEoCdsP7MxJierFsZTEm0zOXd/qTMdDAbes242mUYP4nCjlmPV+Pcid5KBlXzwVkqxf3PhSE8Qp09mQqbb9Z8shlHSESkpHALnodkYmFVHg5ffeKssHZqnu3Qy05WPMLEjrXXVMaqFCFYJ0XlR5S54eDsYRV/lZiYju38MliUBEPgaGQ2Y0rjUPuShomHFEP4FYymeShS/Wt7v1GHerfkuvpLz/HI+WomxMabOEGo5KqAjQyzyW+0UOgX7NXvo6XK2Ujsca8QYTADe+B86sxRqcq46wDhh9j+7XlnbbRle6FRG/PjG9tE0SieNCmYnmDUaOh8apGRvP7srhA6WAx/QZMIW4J3ffAO8Dq9ZEdGsR0BQA+yq+d40aWpAoIwZSJ8YCkRHfm5vRPWZceazNKxD8=";
-      signByDefault = true;
-      format = "ssh";
-      signer = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    settings = {
+      commit.gpgsign = true;
+      user.signingKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkCkwlP4g9fje5DKsPmLv4XYPr+aFreDLJkzoENd0Q4Pjy4HBgucYKoPMP99lqdwbPo/F8AbXVSGX/xe5Jq41ptQVwQAAWBAsi0kUtiBpGjEoCdsP7MxJierFsZTEm0zOXd/qTMdDAbes242mUYP4nCjlmPV+Pcid5KBlXzwVkqxf3PhSE8Qp09mQqbb9Z8shlHSESkpHALnodkYmFVHg5ffeKssHZqnu3Qy05WPMLEjrXXVMaqFCFYJ0XlR5S54eDsYRV/lZiYju38MliUBEPgaGQ2Y0rjUPuShomHFEP4FYymeShS/Wt7v1GHerfkuvpLz/HI+WomxMabOEGo5KqAjQyzyW+0UOgX7NXvo6XK2Ujsca8QYTADe+B86sxRqcq46wDhh9j+7XlnbbRle6FRG/PjG9tE0SieNCmYnmDUaOh8apGRvP7srhA6WAx/QZMIW4J3ffAO8Dq9ZEdGsR0BQA+yq+d40aWpAoIwZSJ8YCkRHfm5vRPWZceazNKxD8=";
+      gpg = {
+        format = "ssh";
+        ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
     };
   };
 
